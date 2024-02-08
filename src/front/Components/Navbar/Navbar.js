62f1bd877dc0193f4e33a85cf3716d7d0310f216
Ryan <ryanpinas77@gmail.com>
2024-02-08 10:50:16 +0100

    update navbar

--- src/front/Components/Navbar/Navbar.js
+++ src/front/Components/Navbar/Navbar.js
@@ -6,13 +6,13 @@ function Navbar() {
         <nav className="fixed top-0 flex items-center flex-wrap bg-gradient-to-r from-teal-500 to-teal-300 p-6 w-full z-50">            
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="flex justify-around text-lg font-bold lg:flex-grow">
-                    <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
+                    <Link to="/" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white transition duration-10000 ease-in-out">
                         Accueil
                     </Link>
-                    <Link to="/docs" className="block mt-4 lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
+                    <Link to="/docs" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
                         Documentation
                     </Link>
-                    <Link to="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
+                    <Link to="/about" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
                         À propos
                     </Link>
                 </div>
