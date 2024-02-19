bf8143d2067cd03a967753a178f874c2cbe54668
Ryan <ryanpinas77@gmail.com>
2024-02-19 12:36:45 +0100

    menu burger

--- src/front/Components/Navbar/Navbar.js
+++ src/front/Components/Navbar/Navbar.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+import { IoIosMenu } from "react-icons/io";
+Modal.setAppElement('#root'); // This line is needed for accessibility reasons
 
 function Navbar() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     return (
-        <nav className="fixed top-0 flex items-center flex-wrap bg-gradient-to-r from-teal-500 to-teal-300 p-6 w-full z-50">            
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <nav className="fixed top-0 flex items-center flex-wrap bg-transparent lg:bg-gradient-to-r from-teal-500 to-teal-300 p-6 w-full z-50">
+            <IoIosMenu className='w-10 h-auto text-white lg:hidden block'
+                onClick={() => setModalIsOpen(!modalIsOpen)}
+            />
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setModalIsOpen(false)}
+                contentLabel="Menu"
+                className="fixed inset-0 flex items-center justify-center outline-none"
+                overlayClassName="fixed inset-0 bg-black bg-opacity-25"
+            >
+                <div className="bg-white  w-[250px] h-[250px] text-2xl rounded-xl flex flex-col items-center justify-center p-5 overflow-auto mx-auto">
+                    <Link to="/" className="text-teal-800 hover:text-white mb-4">
+                        Accueil
+                    </Link>
+                    <Link to="https://github.com/N95Ryan/disgovary#stack-%EF%B8%8F" className="text-teal-800 hover:text-white mb-4">
+                        Documentation
+                    </Link>
+                    <Link to="/about" className="text-teal-800 hover:text-white">
+                        À propos
+                    </Link>
+                </div>           
+            </Modal>            
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:block hidden">
                 <div className="flex justify-around text-lg font-bold lg:flex-grow">
                     <Link to="/" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white transition duration-10000 ease-in-out">
                         Accueil
                     </Link>
-                    <Link to="/docs" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
+                    <Link to="https://github.com/N95Ryan/disgovary#stack-%EF%B8%8F" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
                         Documentation
                     </Link>
                     <Link to="/about" className="block lg:inline-block lg:mt-0 text-teal-800 hover:text-white">
@@ -20,5 +47,4 @@ function Navbar() {
         </nav>
     );
 }
-
 export default Navbar;
--- src/front/Pages/Homepage/Homepage.js
+++ src/front/Pages/Homepage/Homepage.js
@@ -15,14 +15,11 @@ function Homepage() {
           </a>
           <div className="flex flex-col items-center justify-center">
             <p className="text-4xl font-bold text-white mb-4 animate-fade-right animate-once animate-normal">Bienvenue sur Dis<span className="text-teal-400 font-extrabold italic">GO</span>vary.</p>
-            <p className="text-xl text-white animate-fade-left animate-once animate-normal">Veuillez cliquer sur l'un des boutons ci-dessous afin de démarrer.</p>
+            <p className="text-xl text-white animate-fade-left animate-once animate-normal">Veuillez cliquer sur le bouton ci-dessous afin de démarrer.</p>
           </div>
           <div className="flex flex-row items-center justify-center space-x-6">
             <a href="/actions" className="bg-teal-600 hover:bg-teal-800 text-white text-2xl font-bold py-4 px-4 rounded-3xl mt-8 shadow-xl animate-jump animate-once animate-normal">
               C.R.U.D
-            </a>
-            <a href="/sandbox" className="bg-teal-600 hover:bg-teal-800 text-white text-2xl font-bold py-4 px-4 rounded-3xl mt-8 shadow-xl animate-jump animate-once animate-normal">
-              Sandbox
             </a>
 
           </div>
